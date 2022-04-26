1e30f7430bc91b43fa6ae9edc5fe1de0e422ffaa
Pavel Procházka <pavel.prochazka@menier.cz>
2022-04-26 15:08:44 +0200

    Initial public commit under LGPL.

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # octo-compose
-Templating for docker-compose.yml including deploy machine initialization, git support, directories inclusion and volume generating, Docker Swarm support.
+> Tool designed to join docker-compose files into one, templating support included.
+
+# Table of Contents
+* [Installation](#installation)
+* [File description](#file-description)
+  * [cluster-compose.yml](#cluster-composeyml)
+  * [octo-compose.yml](#octo-composeyml)
+  * [Merge process](#merge-process)
+* [Usage](#usage)
+* [Links](#links)
+
+# Installation
+```sh
+git clone https://10.0.0.105:10443/octopus/octo-compose
+npm i octo-compose/ -g
+```
+
+# File description
+## cluster-compose.yml
+```yml
+services:
+  <service 1>:
+    octo-compose: # Optional key
+      # Optionally specify git url from which should be the service cloned
+      git: <url>
+      # Optionally specify git branch or tag to checkout
+      branch: <branch/tag name>
+      # Defaults to "."
+      context: <path/to/service>
+      # Defaults to "octo-compose.yml"
+      octo-compose: <custom octo-compose.yml>
+    # Optional???
+    octo-deploy:
+      # Defaults to 1???
+      replicas: <number of replicas>
+      # Define on which machines can the service run. This is ignored when swarm
+      # mode is disabled.
+      placement:
+        - <tag 1>
+        ...
+        - <tag N>
+      port-range:
+        # Either a single number (if number of replicas is 1) or a range of
+        # ports:
+        low: <number>
+        high: <number>
+      # Docker image registry
+      registry: <ip:port>
+    octo-host-prepare:
+      - "bash script 1" # Templating vars can be contained
+      ...
+      - "bash script N"
+    docker-compose-key1: docker-compose-value1 # Value can also contain templating vars ${...}
+    ...
+    docker-compose-keyN: docker-compose-valueN
+  <service N>:
+  ...
+```
+Note that cluster-compose.yml does not need to include any octo-compose.yml files. Typically you don't need to create octo-compose.yml files for 3rd party images.
+
+
+## octo-compose.yml
+A regular docker-compose file with custom templating vars written as `${VAR}`:
+
+* `CONTEXT` - Equals to `service.octo-compose.context`
+* `INSTANCE_ID` - The ID of the instance
+* `INSTANCE_NAME` - Equals to `service_<INSTANCE_ID>`
+* `INSTANCE_PORT` - The port assigned to the service instance from `service.octo-deploy.port-range`
+* `INSTANCE_REGISTRY` - Equals to `service.octo-deploy.registry`
+* `INSTANCE_TARGET_MACHINE` - Final cluster placement from `service.octo-deploy.placement`
+
+Note that list of the temaplating vars is extended by **environment** variables.
+
+## Merge process
+All keys except for `octo-compose` and `octo-deploy` (and their child-keys) are merged recursively. Keys that come first in the merge process have higher priority, e.g. if `cluster-compose.yml` defines ENV `example_1=test_value_1` of `service_a` and corresponding `octo-compose.yml` of the service defines `example_1=test_value_2`, then `test_value_1` is used.
+
+# Host initialization and preparation
+In certain situation users need to prepare host machine to a specific state e.g. create specific volume directories, set specific user to certain files etc. For these cases you can specify initialization steps within 'octo-host-prepare' list. Each value is expected to be a valid *bash* script, order of execution is from top to bottom and in synchronous manner. Host preparation is done by this command:
+```
+$ octo-compose -i my-cluster-compose.yml -hostPrepare # or simply -p
+```
+
+# Usage
+Octo-compose takes `cluster-compose.yml` as an input and outputs a regular `docker-compose.yml` which you can run using standard docker-compose commands - either in swarm mode or non-swarm mode.
+
+Run `octo-compose -h` to display the help message for more info.
+
+# Links
+* [Docker Compose Overview](https://docs.docker.com/compose/)
---
+++ index.js
@@ -0,0 +1,301 @@
+#!/usr/bin/env node
+/* jshint esversion: 6 */
+const fs = require("fs");
+const path = require("path");
+const yml = require("js-yaml");
+const _ = require("underscore");
+const { spawnSync, execSync } = require('child_process');
+
+const DEFAULT_IN = 'cluster-compose.yml';
+
+var noSwarmP = false;
+var forcedBranch = undefined;
+var prepareHostP = false;
+
+function gitFetch(url, extraArgs) {
+  extraArgs = extraArgs || "";
+  const project = path.basename(url, ".git");
+
+  let ret = undefined;
+
+  if (fs.statSync(project, { throwIfNoEntry: false }) === undefined) {
+    ret = spawnSync("git", ["clone", `${url}${extraArgs}`, '--recurse-submodules'], { env: process.env });
+  }
+  else {
+    ret = spawnSync('git', ['pull'], { env: process.env, cwd: project });
+  }
+
+  if (ret.status !== 0) {
+    console.log(`Status non-0: ${ret.status}`);
+    if (ret.stderr)
+      console.error(ret.stderr.toString());
+    if (ret.stdout)
+      console.log(ret.stdout.toString());
+    throw new Error(`Git returned an error code ${ret.status}`);
+  }
+
+  return project;
+}
+
+function gitCheckout(project, rev) {
+  const ret = spawnSync("git", ["checkout", rev], { cwd: project });
+
+  if (ret.status !== 0) {
+    if (ret.stderr)
+      console.error(ret.stderr.toString());
+    if (ret.stdout)
+      console.log(ret.stdout.toString());
+
+    throw new Error(`Git returned an error code ${ret.status}`);
+  }
+
+  return project;
+}
+
+function merge(a, b) {
+  if (_.isArray(a) && _.isArray(b)) {
+    return _.union(a, b);
+  }
+  else if (_.isObject(b) && _.isObject(a)) {
+    let out = _.extend({}, a);
+    for (let k in b) {
+      if (a[k]) {
+        out[k] = merge(a[k], b[k]);
+      }
+      else {
+        out[k] = b[k];
+      }
+    }
+    return out;
+  }
+
+  return b;
+}
+
+function expandServiceTemplate(serviceCompose, translation) {
+  function mapping(value) {
+    if (_.isArray(value)) {
+      return _.map(value, mapping);
+    }
+    else if (_.isObject(value)) {
+      return _.mapObject(value, mapping);
+    }
+    else if (_.isString(value)) {
+      let out = value;
+
+      for (const tr in translation) {
+        out = out.replaceAll(`$\{${tr}\}`, translation[tr]);
+      }
+
+      return out;
+    }
+
+    return value;
+  }
+
+  return _.mapObject(serviceCompose, mapping);
+}
+
+function fetchService(octoCompose) {
+  const oc = octoCompose;
+
+  if (!oc) {
+    return undefined;
+  }
+
+  if (oc.git) {
+    const project = gitFetch(oc.git);
+    gitCheckout(project, forcedBranch || oc.branch || "master");
+  }
+
+  const octoFileName = path.join(octoCompose.context || ".", octoCompose["octo-compose"] || "octo-compose.yml");
+  fs.statSync(octoFileName);
+  return octoFileName;
+}
+
+function expandOctoCompose(octoDeploy, octoCompose, parentalDockerCompose, parentalDockerServiceName) {
+  const octoFileName = fetchService(octoCompose);
+  let octoFileContent = undefined;
+
+  if (octoFileName) {
+    octoFileContent = fs.readFileSync(octoFileName, 'utf8');
+  }
+
+  let octoFileParsed = (octoFileContent ? yml.load(octoFileContent) : { [parentalDockerServiceName]: {} });
+  octoCompose = octoCompose || {};
+  // console.error("OCTOFIENAME:::", octoFileParsed);
+
+  /**
+  *  octo-deploy:
+      replicas: 1
+      placement:
+        - manager
+      port-range: 7777
+  */
+  let replicas = octoDeploy.replicas || 1;
+  let placement = octoDeploy.placement;
+  if (!placement || placement.length == 0) {
+    throw new Error("Key placement must be present and must have at least one index.");
+  }
+
+  let portRange = octoDeploy["port-range"];
+
+  if (portRange) {
+    if (_.isObject(portRange)) {
+      const low = portRange.low;
+      if (!low) throw new Error("Low range must be specified");
+      const high = portRange.high;
+      if (!high) throw new Error("High range must be specified");
+      if (low > high) throw new Error("Low must be lower than High");
+    }
+    else if (_.isNumber(portRange)) {
+      portRange = { low: portRange, high: portRange };
+    }
+  }
+
+  let iRegistry = (noSwarmP ? "" : octoDeploy.registry || "");
+  if (iRegistry.length > 0) {
+    iRegistry += "/";
+  }
+
+  let trans = {
+    INSTANCE_ID: _.range(replicas),
+    INSTANCE_PORT: portRange ? _.range(portRange.low, portRange.high + 1) : undefined,
+    INSTANCE_TARGET_MACHINE: placement.slice(),
+    INSTANCE_REGISTRY: iRegistry,
+    CONTEXT: octoCompose.context
+  };
+
+  if (portRange && trans.INSTANCE_ID.length > trans.INSTANCE_PORT.length) {
+    throw new Error("Allocated port range must be wider than replicas count");
+  }
+
+  let services = {};
+
+  /** generate all service replicas */
+  for (let i = 0; i < replicas; i++) {
+    let composeResult = {};
+
+    const INSTANCE_ID = trans.INSTANCE_ID.shift();
+    const INSTANCE_PORT = portRange ? trans.INSTANCE_PORT.shift() : undefined;
+    const INSTANCE_TARGET_MACHINE = trans.INSTANCE_TARGET_MACHINE.shift();
+    const CONTEXT = trans.CONTEXT;
+    const INSTANCE_REGISTRY = trans.INSTANCE_REGISTRY;
+
+    trans.INSTANCE_TARGET_MACHINE.push(INSTANCE_TARGET_MACHINE);
+
+    /** for each service translate a) service name b) replace tags of form ${TAG} */
+    for (const service in octoFileParsed) {
+      /** Default tags */
+      const serviceName = (replicas > 1 ? service + `_${INSTANCE_ID}` : service);
+
+      let instanceTrans = {
+        CONTEXT: CONTEXT,
+        INSTANCE_ID: INSTANCE_ID,
+        INSTANCE_PORT: INSTANCE_PORT,
+        INSTANCE_TARGET_MACHINE: INSTANCE_TARGET_MACHINE,
+        INSTANCE_NAME: serviceName,
+        INSTANCE_REGISTRY: INSTANCE_REGISTRY
+      };
+      /** User defined tags - use from environment */
+      instanceTrans = _.extend(instanceTrans, process.env);
+
+      /** Update octoFileParsed by parentalDockerCompose */
+      const updatedOctoFileParsed = merge(octoFileParsed[service], parentalDockerCompose);
+
+      /** Collect results */
+      const expService = expandServiceTemplate(updatedOctoFileParsed, instanceTrans);
+
+      composeResult[serviceName] = expService;
+    }
+
+    /** merge composeResult to services */
+    services = _.extend(services, composeResult);
+  }
+
+  return services;
+}
+
+function expandClusterCompose(clusterComposePath) {
+  let clusterFileContent = fs.readFileSync(clusterComposePath, 'utf8');
+  // console.error(clusterFileContent);
+  let clusterFileParsed = yml.load(clusterFileContent);
+
+  let out = _.extend({}, clusterFileParsed);
+  out.services = {}; /** get rid of services - rebuild is done now */
+
+  for (const s in clusterFileParsed.services) {
+    const cService = clusterFileParsed.services[s];
+    const octoCompose = cService["octo-compose"];
+    const octoDeploy = cService["octo-deploy"];
+    const parentalDockerCompose = (prepareHostP ? _.clone(cService) : _.omit(cService, "octo-compose", "octo-deploy", "octo-host-prepare"));
+
+    _.extendOwn(out.services, expandOctoCompose(octoDeploy, octoCompose, parentalDockerCompose, s));
+  }
+
+  return out;
+}
+
+function runScript(code, envExtenstion) {
+  envExtenstion = envExtenstion || {};
+
+  try {
+    execSync(code, { env: _.extend(process.env, envExtenstion), stdio: 'inherit' });
+  } catch(e) {
+    console.error(`Command execution error: ${e.message}`);
+    throw new Error(`Script thrown: ${e.message}`);
+  }
+}
+
+function main(conf) {
+  const clusterComposePath = conf["-i"] || conf["--input"] || DEFAULT_IN;
+  const destComposePath = conf["-o"] || conf["--output"] || 1; /** TRICK: 1 = stdout */
+  noSwarmP = conf["-n"] || conf["--noSwarm"] || false;
+  prepareHostP = conf["-p"] || conf["--hostPrepare"]
+  const clusterJSON = expandClusterCompose(clusterComposePath);
+
+  if (noSwarmP) {
+    /** remove .deploy key from services */
+    _.each(clusterJSON.services, function (service) { delete service.deploy; });
+  }
+
+  if (prepareHostP) {
+    console.log(JSON.stringify(clusterJSON));
+    _.each(clusterJSON.services, function(service) {
+      let serviceEnv = {};
+      _.each(service.environment || [], function(env){
+        const envSplit = env.split("=");
+        serviceEnv[envSplit[0]] = envSplit[1];
+      });
+
+      let cmds = service["octo-host-prepare"];
+      _.each(cmds, _.partial(runScript, _, serviceEnv));
+    });
+  } else {
+    const clusterYML = yml.dump(clusterJSON, { noRefs: true });
+    fs.writeFileSync(destComposePath, clusterYML);
+  }
+}
+
+let conf = {};
+let args = process.argv.slice(2);
+for (let a = 0; a < args.length; a++) {
+  if ((a % 2) == 0) {
+    conf[args[a]] = args[a + 1];
+  }
+}
+
+// console.error("ARGS::::", conf);
+
+if (conf.hasOwnProperty("--help") || conf.hasOwnProperty("-h")) {
+  console.log(
+    "Usage: octo-compose [-p|--hostPrepare] [-i|--input] [-o|--output] [--noSwarm|-n] [--help|-h]\n\n"
+    + `  --input    Path to the input cluster-compose.yml file. Defaults to "${DEFAULT_IN}".\n`
+    + "  --output   Path to the output docker-compose.yml file. Defaults to stdout.\n"
+    + "  --noSwarm  Use true to disable swarm. Defaults to false.\n"
+    + "  --hostPrepare Use true value to start host initialization scripts defined under 'octo-host-prepare' key in cluster-compose.yml. May be set to true only. Stdout is used as inherited scripts stdout then. When used -o argument, stdout of scripts will be its content."
+    + "  --help     Print this message and exit.\n");
+  process.exit();
+}
+
+main(conf);
